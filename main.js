9649c36de77ef15145c33c3c23ee2fa1d90051f4
Mu-Le Lee <s094392@gmail.com>
2025-02-02 00:35:25 +0800

    third

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const sc = new ScratchCard('#js--sc--container', {
   containerWidth: scContainer.offsetWidth,
   containerHeight: scContainer.offsetHeight,
   imageForwardSrc: 'images/scratchcard.jpg',
-  imageBackgroundSrc: 'images/background.jpg',
+  imageBackgroundSrc: 'images/pusheen.jpg',
   htmlBackground: '',
   clearZoneRadius: scContainer.offsetWidth / 15,
   nPoints: 0,
@@ -20,7 +20,7 @@ sc.init().then(() => {
   alert(error.message);
 });
 
-const shardButton = document.getElementById('shard_button')
+const shardButton = document.getElementById('share_button')
 shardButton.addEventListener('click', () => {
   if (navigator.share) {
     navigator.share({
@@ -31,4 +31,14 @@ shardButton.addEventListener('click', () => {
       .then(() => console.log('成功！'))
       .catch((error) => console.log('發生錯誤', error));
   }
+})
+
+const facebookButton = document.getElementById('facebook_button')
+facebookButton.addEventListener('click', () => {
+  window.open('https://www.facebook.com/%E6%96%B0%E8%8E%8A%E9%9D%88%E7%B3%A7%E5%A0%82LLC-Hsinchuang-237835819608260/', '_blank')
+})
+
+const timeButton = document.getElementById('time_button')
+timeButton.addEventListener('click', () => {
+  window.open('http://www.jesusloves.tw/2603233674387283197522530.html', '_blank')
 })
